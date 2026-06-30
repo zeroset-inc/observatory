5a6b0fe3e45f61af22f4d7e5b40ffb4c382f8055
Sajjaad Farzad <sajjaad@zeroset.com>
2026-06-30 15:15:49 -0700

    add cloudflare builder (#59)

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   envDir: path.resolve(__dirname, ".."),
   resolve: {
+    preserveSymlinks: true,
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
